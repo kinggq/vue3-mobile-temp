f3027a48776f5560c013aef2c9459549c8081fd1
kinggq <18701094030@163.com>
2023-09-21 16:03:49 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <main>
+    <van-button size="large" type="primary">
+      button
+    </van-button>
+    <van-button type="success">
+      success button
+    </van-button>
+    <RouterView />
+    <TheFooter />
+  </main>
+</template>
